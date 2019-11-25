835f2a58c4cffd9e61212d89550660c1b77b3538
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-25 11:57:00 +0100

    BeEs BeEs BeEs

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
 var ForagerBee = function() {
+	Bee.call(this)
+	this.job           = 'find pollen';
+	this.color         = 'yellow' ;
+	this.treasureChest = []
 };
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
 
+ForagerBee.prototype.forage = function( treasure ) {
+	this.treasureChest.push(treasure)
+}
